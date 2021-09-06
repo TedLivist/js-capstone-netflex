e57fe2c85b7719bda1a4e376a8722149c0189091
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-09-06 23:37:19 +0100

    Add getShows function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import './assets/stylesheets/style.css'
+import './assets/stylesheets/style.css';
+import getShows from './modules/getShows.js';
 
-console.log('Hahahahahaha')
+const showsContainer = document.querySelector('.shows-list')
+
+window.addEventListener('load', async () => {
+  const shows = await getShows()
+  const sixShows = shows.slice(0, 6)
+  console.log(sixShows)
+
+  console.log(showsContainer)
+})
